83946b33b33c2535a6e4de5dfdb28889f07a4a82
chidangao2013 <reg2980@163.com>
2022-01-22 16:05:12 +0800

    用户端nuxt框架搭建，首页引入接口调试

---
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/api/HospApiController.java
@@ -0,0 +1,65 @@
+package com.atguigu.yygh.hosp.controller.api;
+
+
+import com.atguigu.yygh.common.result.Result;
+import com.atguigu.yygh.hosp.service.DepartmentService;
+import com.atguigu.yygh.hosp.service.HospitalService;
+import com.atguigu.yygh.model.hosp.Hospital;
+import com.atguigu.yygh.vo.hosp.DepartmentVo;
+import com.atguigu.yygh.vo.hosp.HospitalQueryVo;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/api/hosp/hospital")
+@RestController
+public class HospApiController {
+
+    @Autowired
+    private HospitalService hospitalService;
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @ApiOperation(value = "查询医院列表")
+    @GetMapping("findHospList/{page}/{limit}")
+    public Result findHospList(@PathVariable Integer page,
+                               @PathVariable Integer limit,
+                               HospitalQueryVo hospitalQueryVo) {
+
+        Page<Hospital> hospitals = hospitalService.selectHospPage(page, limit, hospitalQueryVo);
+        List<Hospital> content = hospitals.getContent();
+        int totalPages = hospitals.getTotalPages();
+
+        return Result.ok(hospitals);
+    }
+
+
+    @ApiOperation(value = "根据医院名称查询")
+    @GetMapping("findByHosname/{hosname}")
+    public Result findByHosname(@PathVariable String hosname) {
+       List<Hospital> list = hospitalService.findByHosname(hosname);
+       return Result.ok(list);
+
+    }
+    @ApiOperation(value = "根据医院编号获取科室")
+    @GetMapping("department/{hoscode}")
+    public  Result  index(@PathVariable String hoscode) {
+        List<DepartmentVo> deptTree = departmentService.findDeptTree(hoscode);
+        return Result.ok(deptTree);
+    }
+    @ApiOperation(value = "根据医院编号，获取医院预约挂号详情")
+    @GetMapping("findHospDetail/{hoscode}")
+    public Result item (@PathVariable String hoscode){
+        Map<String,Object > map = hospitalService.item(hoscode);
+        return Result.ok(map);
+    }
+}
--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/HospitalServiceImpl.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/HospitalServiceImpl.java
@@ -3,6 +3,7 @@ package com.atguigu.yygh.hosp.service.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.yygh.cmn.client.DictFeignClient;
 import com.atguigu.yygh.hosp.repository.HospitalRepository;
+import com.atguigu.yygh.hosp.service.DepartmentService;
 import com.atguigu.yygh.hosp.service.HospitalService;
 import com.atguigu.yygh.model.hosp.Hospital;
 import com.atguigu.yygh.vo.hosp.HospitalQueryVo;
@@ -25,6 +26,9 @@ public class HospitalServiceImpl  implements HospitalService {
     @Autowired
     private DictFeignClient dictFeignClient;
 
+    @Autowired
+    private DepartmentService departmentService;
+
     @Override
     public void save(Map<String, Object> switchMap) {
         //首先将参数map集合转换成对象Hospital
@@ -34,14 +38,14 @@ public class HospitalServiceImpl  implements HospitalService {
         String hoscode = hospital.getHoscode();
         Hospital hospitalExist = hospitalRepository.getHospitalByHoscode(hoscode);
         //如果存在，进行修改
-        if(hospitalExist !=null) {
+        if (hospitalExist != null) {
             hospital.setStatus(hospitalExist.getStatus());
             hospital.setCreateTime(hospitalExist.getCreateTime());
             hospital.setUpdateTime(new Date());
             hospital.setIsDeleted(0);
             hospitalRepository.save(hospital);
 
-        }else {
+        } else {
             //如果不存在，进行添加
             //0：未上线 1：已上线
             hospital.setStatus(0);
@@ -60,32 +64,32 @@ public class HospitalServiceImpl  implements HospitalService {
         Hospital hospital = hospitalRepository.getHospitalByHoscode(hoscode);
         return hospital;
     }
+
     //医院列表(条件查询分页)
     @Override
     public Page<Hospital> selectHospPage(Integer page, Integer limit, HospitalQueryVo hospitalQueryVo) {
         //创建pageable对象
-        Pageable pageable   = PageRequest.of(page - 1, limit);
+        Pageable pageable = PageRequest.of(page - 1, limit);
         //构建条件匹配器
         ExampleMatcher matcher = ExampleMatcher.matching()
                 .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING)
                 .withIgnoreCase(true);
         //hospitalQueryVo转换成Hospital对象
         Hospital hospital = new Hospital();
-        BeanUtils.copyProperties(hospitalQueryVo,hospital);
+        BeanUtils.copyProperties(hospitalQueryVo, hospital);
         //创建对象
-        Example<Hospital> example = Example.of(hospital,matcher);
+        Example<Hospital> example = Example.of(hospital, matcher);
         //调用
         Page<Hospital> pages = hospitalRepository.findAll(example, pageable);
 
 //        List<Hospital> content = pages.getContent();
 
         //获取查询list集合，遍历进行医院等级封装
-        pages.getContent().stream().forEach(item->{
+        pages.getContent().stream().forEach(item -> {
             this.setHospitalHosType(item);
         });
         return pages;
     }
-
 
 
     private Hospital setHospitalHosType(Hospital hospital) {
@@ -97,11 +101,12 @@ public class HospitalServiceImpl  implements HospitalService {
         String district = dictFeignClient.getName(hospital.getDistrictCode());
 
 
-        hospital.getParam().put("fullAddress",province+city+district);
+        hospital.getParam().put("fullAddress", province + city + district);
 
-        hospital.getParam().put("hostypeString",hostypeString);
+        hospital.getParam().put("hostypeString", hostypeString);
         return hospital;
     }
+
     //更新医院状态
     @Override
     public void updateStatus(String id, Integer status) {
@@ -113,6 +118,7 @@ public class HospitalServiceImpl  implements HospitalService {
         hospitalRepository.save(hospital);
 
     }
+
     //获取医院详细信息
     @Override
     public Map<String, Object> getHospById(String id) {
@@ -131,11 +137,30 @@ public class HospitalServiceImpl  implements HospitalService {
     @Override
     public String getHospName(String hoscode) {
         Hospital hospital = hospitalRepository.getHospitalByHoscode(hoscode);
-        if(hospital != null){
+        if (hospital != null) {
             return hospital.getHosname();
         }
         return null;
     }
 
+    @Override
+    public List<Hospital> findByHosname(String hosname) {
+        return hospitalRepository.findHospitalByHosnameLike(hosname);
+    }
 
+    //根据医院编号，获取医院预约挂号详情
+    @Override
+    public Map<String, Object> item(String hoscode) {
+        Map<String, Object> result = new HashMap<>();
+        //医院详情
+        Hospital hospital = this.setHospitalHosType(this.getByHoscode(hoscode));
+        result.put("hospital", hospital);
+        //预约规则
+        result.put("bookingRule", hospital.getBookingRule());
+        //不需要重复返回
+        hospital.setBookingRule(null);
+        return result;
+
+
+    }
 }
